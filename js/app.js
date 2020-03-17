c94963ba83d2f7025c6d33e100a5eb70ddefd3d2
meli castorena <mcastorena0316@gmail.com>
2020-03-17 11:39:39 -0600

    create javascript logic for adding books and display them

---
+++ js/app.js
@@ -0,0 +1,32 @@
+function Book(title,author, pages, read) {
+    this.title= title;
+    this.author= author;
+    this.pages= pages;
+    this.read= read;
+}
+
+const add_button=  document.querySelector("#add-book-button")
+add_button.addEventListener("click", addBookToLibrary )
+let myLibrary = [];
+
+
+function addBookToLibrary(e) {   
+  const title= document.getElementById("title").value;
+  const author= document.getElementById("author").value;
+  const pages= document.getElementById("pages").value;
+  const read= document.getElementById("read").checked;
+  const book= new Book(title,author,pages,read);
+  myLibrary.push(book);
+  let info= document.createElement("tr")
+  let book_content= `
+                    <th scope="row">${book.title}</th>
+                    <td>${book.author}</td>
+                    <td>${book.pages}</td>
+                    <td>${book.read}</td>`
+  info.innerHTML= book_content;
+  document.getElementById("book-info-body").append(info)
+  e.preventDefault();
+}
+
+
+
